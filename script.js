d700211256d2bedf3d705ea2ab2537dab420334b
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-07 23:32:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+	let lower = str.toLowerCase()
+	let rev = lower.split(" ").reverse().join(" ")
+	if(rev === lower){
+		return true
+	}else{
+		return false
+	}
 }
 module.exports = palindrome
