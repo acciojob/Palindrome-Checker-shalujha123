cb9eea06dfc3f9ef9cca590dec5d25a53e8e562f
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-07 23:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-	let lower = str.toLowerCase()
-	let rev = lower.split(" ").reverse().join(" ")
-	if(rev === lower){
-		return true
-	}else{
-		return false
-	}
+	let lower = str.toLowerCase();
+
+  // Step 2: Remove non-alphanumeric characters using regex
+  let cleaned = lower.replace(/[^a-z0-9]/g, '');
+
+  // Step 3: Reverse the cleaned string
+  let reversed = cleaned.split('').reverse().join('');
+
+  // Step 4: Compare
+  return cleaned === reversed;
+	
 }
 module.exports = palindrome
